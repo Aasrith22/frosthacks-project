ab678554879cccd690fae94924133cd82500778d
Aasrith22 <147178544+Aasrith22@users.noreply.github.com>
2024-01-21 16:33:52 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,107 @@
+
+  <?php include 'header.php';?>
+  <section class="main-banner">
+    <div class="tp-banner-container">
+      <div class="tp-banner">
+        <ul>
+          
+          <li data-transition="random" data-slotamount="7" data-masterspeed="300"  data-saveperformance="off" > 
+            <img src="images/hms.jpg"  alt="slider"  data-bgposition=" center center" data-bgfit="cover" data-bgrepeat="no-repeat"> 
+            
+            <div class="tp-caption sfl tp-resizeme" 
+                data-x="center" data-hoffset="0" 
+                data-y="center" data-voffset="-120" 
+                data-speed="800" 
+                data-start="800" 
+                data-easing="Power3.easeInOut" 
+                data-splitin="chars" 
+                data-splitout="none" 
+                data-elementdelay="0.03" 
+                data-endelementdelay="0.4" 
+                data-endspeed="300"
+                style="z-index: 5; font-size:50px; font-weight:500; color:black;  max-width: auto; max-height: auto; white-space: nowrap;">Hospital name and photos are displayed here</div>
+            
+            <div class="tp-caption sfr tp-resizeme" 
+                data-x="center" data-hoffset="0" 
+                data-y="center" data-voffset="-60" 
+                data-speed="800" 
+                data-start="1000" 
+                data-easing="Power3.easeInOut" 
+                data-splitin="chars" 
+                data-splitout="none" 
+                data-elementdelay="0.03" 
+                data-endelementdelay="0.1" 
+                data-endspeed="300" 
+                style="z-index: 6; font-size:40px; color:black; font-weight:500; white-space: nowrap;">(For advertisement purposes) </div>
+            
+          </li>
+          
+          
+  </section>
+  
+  <div id="content"> 
+    
+    <section class="p-t-b-150">
+      <div class="container">
+        <div class="intro-main">
+          <div class="row"> 
+            
+            
+            <div class="col-md-4">
+              <div class="timing"> <i class="lnr lnr-clock"></i>
+                <ul>
+                <h6>THIS IS FOR POSTING TIMINGS</h6>
+                  <li> Monday <span>9.00 - 19.00</span></li>
+                  <li> Tuesday <span>9.00 - 19.00</span></li>
+                  <li> Wednesday <span>9.00 - 19.00</span></li>
+                  <li> Thursday <span>9.00 - 19.00</span></li>
+                  <li> Friday <span>9.00 - 19.00</span></li>
+                  <li> Saturday <span>9.00 - 19.00</span></li>
+                  <li> Sunday <span>9.00 - 15.00</span></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    
+  
+
+    
+    <section class="p-t-b-150">
+      <div class="container"> 
+        
+        <div class="heading-block">
+          <h2>Doctors list and services provided can be listed here</h2>
+          
+        
+    </section>
+   
+    
+    
+  </div>
+  <script src="https://cdn.botpress.cloud/webchat/v1/inject.js"></script>
+  <script src="https://cdn.botpress.cloud/webchat/v1/inject.js"></script>
+<script>
+  window.botpressWebChat.init({
+      "composerPlaceholder": "Chat with Med-bot",
+      "botConversationDescription": "Please drop down your Queries",
+      "botId": "1fdb6ab5-ca4c-40d5-b07c-75b8b29d93e0",
+      "hostUrl": "https://cdn.botpress.cloud/webchat/v1",
+      "messagingUrl": "https://messaging.botpress.cloud/",
+      "clientId": "1fdb6ab5-ca4c-40d5-b07c-75b8b29d93e0",
+      "webhookId": "dd5d54ba-f2ad-4665-8215-080701ea5a93",
+      "lazySocket": true,
+      "themeName": "prism",
+      "botName": "Med-bot",
+      "avatarUrl": "https://cdn-icons-png.flaticon.com/512/2021/2021706.png",
+      "stylesheet": "https://webchat-styler-css.botpress.app/prod/f7183785-a2da-42dd-990f-38fac9cb799a/v58733/style.css",
+      "frontendVersion": "v1",
+      "useSessionStorage": true,
+      "theme": "prism",
+      "themeColor": "#2563eb"
+  });
+</script>
+  <!-- Footer -->
+<?php include 'footer.php';?>
